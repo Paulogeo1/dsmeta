4ce9864f86f37c4e212a047a098c08039eda852e
Paulo Eduardo <paulo.pauloeduardo@hotmail.com>
2022-09-11 23:44:35 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { toast } from 'react-toastify';
 import icon from '../../assets/img/notification-icon.svg'
 import { BASE_URL } from '../../utils/request';
 
@@ -12,7 +13,7 @@ type Props = {
 function handleClick(salesId :number) {
     axios(`${BASE_URL}/sales/${salesId}/notification`)
         .then(response =>{
-            console.log("SUCESSO")
+            toast.info("SMS enviado com sucesso");
         })
 }
 
